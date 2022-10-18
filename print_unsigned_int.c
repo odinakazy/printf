7503c8cddeacf5a14a60b3a9e6fa7185f122fb7d
odinakazy <odinakasolomon4@gmail.com>
2022-10-18 15:19:49 -0700

    so better

--- print_unsigned_int.c
+++ print_unsigned_int.c
@@ -8,28 +8,28 @@
  */
 int print_u(va_list u)
 {
-  unsigned int a[10];
-  unsigned int i, m, n, sum;
-  int count;
+	unsigned int a[10];
+	unsigned int i, m, n, sum;
+	int count;
 
-  n = va_arg(u, unsigned int);
-  m = 1000000000; /* (10 ^ 9) */
-  a[0] = n / m;
-  for (i = 1; i < 10; i++)
-    {
-      m /= 10;
-      a[i] = (n / m) % 10;
-    }
-  for (i = 0, sum = 0, count = 0; i < 10; i++)
-    {
-      sum += a[i];
-      if (sum || i == 9)
+	n = va_arg(u, unsigned int);
+	m = 1000000000; /* (10 ^ 9) */
+	a[0] = n / m;
+	for (i = 1; i < 10; i++)
 	{
-	  _putchar('0' + a[i]);
-	  count++;
+		m /= 10;
+		a[i] = (n / m) % 10;
 	}
-    }
-  return (count);
+	for (i = 0, sum = 0, count = 0; i < 10; i++)
+	{
+		sum += a[i];
+		if (sum || i == 9)
+		{
+			_putchar('0' + a[i]);
+			count++;
+		}
+	}
+	return (count);
 }
 
 /**
@@ -40,28 +40,28 @@ int print_u(va_list u)
  */
 int print_o(va_list o)
 {
-  unsigned int a[11];
-  unsigned int i, m, n, sum;
-  int count;
+	unsigned int a[11];
+	unsigned int i, m, n, sum;
+	int count;
 
-  n = va_arg(o, unsigned int);
-  m = 1073741824; /* (8 ^ 10) */
-  a[0] = n / m;
-  for (i = 1; i < 11; i++)
-    {
-      m /= 8;
-      a[i] = (n / m) % 8;
-    }
-  for (i = 0, sum = 0, count = 0; i < 11; i++)
-    {
-      sum += a[i];
-      if (sum || i == 10)
+	n = va_arg(o, unsigned int);
+	m = 1073741824; /* (8 ^ 10) */
+	a[0] = n / m;
+	for (i = 1; i < 11; i++)
 	{
-	  _putchar('0' + a[i]);
-	  count++;
+		m /= 8;
+		a[i] = (n / m) % 8;
 	}
-    }
-  return (count);
+	for (i = 0, sum = 0, count = 0; i < 11; i++)
+	{
+		sum += a[i];
+		if (sum || i == 10)
+		{
+			_putchar('0' + a[i]);
+			count++;
+		}
+	}
+	return (count);
 }
 
 /**
@@ -72,27 +72,26 @@ int print_o(va_list o)
  */
 int print_b(va_list b)
 {
-  unsigned int n, m, i, sum;
-  unsigned int a[32];
-  int count;
+	unsigned int n, m, i, sum;
+	unsigned int a[32];
+	int count;
 
-  n = va_arg(b, unsigned int);
-  m = 2147483648; /* (2 ^ 31) */
-  a[0] = n / m;
-  for (i = 1; i < 32; i++)
-    {
-      m /= 2;
-      a[i] = (n / m) % 2;
-    }
-  for (i = 0, sum = 0, count = 0; i < 32; i++)
-    {
-      sum += a[i];
-      if (sum || i == 31)
+	n = va_arg(b, unsigned int);
+	m = 2147483648; /* (2 ^ 31) */
+	a[0] = n / m;
+	for (i = 1; i < 32; i++)
 	{
-	  _putchar('0' + a[i]);
-	  count++;
+		m /= 2;
+		a[i] = (n / m) % 2;
 	}
-    }
-  return (count);
+	for (i = 0, sum = 0, count = 0; i < 32; i++)
+	{
+		sum += a[i];
+		if (sum || i == 31)
+		{
+			_putchar('0' + a[i]);
+			count++;
+		}
+	}
+	return (count);
 }
-
